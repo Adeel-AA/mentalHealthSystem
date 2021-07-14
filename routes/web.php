eab4e4a743f5145b768dacdf02420f2a55c3b1ab
Adeel Ahmed <adeelahmed1995@outlook.com>
2021-07-14 20:37:14 +0100

    Add user authentication, sailwind css and vue

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::middleware('auth')->group(function (){
+//    Route::get
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
