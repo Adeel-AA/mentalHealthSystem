13e5d0f8355bc2104b732e923e645e0d46a24aad
Adeel Ahmed <adeelahmed1995@outlook.com>
2021-07-28 13:08:40 +0100

    Add availability end points

--- app/Models/User.php
+++ app/Models/User.php
@@ -55,4 +55,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Appointment::class);
     }
+
+    public function availability()
+    {
+        return $this->hasMany(Availability::class);
+    }
 }
