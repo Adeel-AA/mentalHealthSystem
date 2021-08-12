063c392f9d3798fecf35fcfaea5e15012043d419
Adeel Ahmed <adeelahmed1995@outlook.com>
2021-08-12 16:10:04 +0100

    Add subtle blue styling on  page headers with tailwind css

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -12,22 +12,25 @@ class UserTest extends TestCase
     use WithFaker;
     use RefreshDatabase;
 
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
+
     public function testUserCanBeCreatedFromValidEmailAddress()
     {
+//        $user = User::factory()->make();
 
-        $user = User::factory()->make();
+        $user = [
+            'name' => $this->faker->name,
+            'email' => $this->faker->email,
+            'password' => 'password123',
+            'password_confirmation' => 'password123'
+        ];
 
-        $this->assertInstanceOf(User::class, $user);
+        $response = $this->post('/register', $user);
+
+        $response->assertRedirect('/home');
     }
 
     public function testUserCannotBeCreatedFromInvalidEmailAddress()
     {
-
         $user = [
             'name' => $this->faker->name,
             'email' => 'invalid',
@@ -35,7 +38,65 @@ class UserTest extends TestCase
         ];
 
         $response = $this->post('/register', $user);
-        $response->assertSessionHasErrors('email');
+        $response->assertSessionHasErrorsIn('email');
+    }
+
+    public function testUserPasswordInsufficientLengthOnRegistration()
+    {
+        $user = [
+            'name' => $this->faker->name,
+            'email' => $this->faker->email,
+            'password' => 'short'
+        ];
+
+        $response = $this->post('/register', $user);
+        $response->assertSessionHasErrorsIn('password');
+
+    }
+
+    public function testUserPasswordDoesNotMatchOnRegistration()
+    {
+        $user = [
+            'name' => $this->faker->name,
+            'email' => $this->faker->email,
+            'password' => 'password123',
+            'password_confirmation' => 'password321'
+        ];
+
+        $response = $this->post('/register', $user);
+        $response->assertSessionHasErrorsIn('password');
+
+    }
+
+    public function testUserCanLogIn()
+    {
+        $user = [
+            'name' => $this->faker->name,
+            'email' => $this->faker->email,
+            'password' => 'password123',
+            'password_confirmation' => 'password123'
+        ];
+
+        $this->post('/register', $user);
+
+        $response = $this->post('/login', $user);
+
+        $response->assertRedirect('/home');
+
+    }
+
+    public function testNonExistingUserCannotLogIn()
+    {
+        $user = [
+            'email' => $this->faker->email,
+            'password' => 'password123',
+        ];
+
+
+        $response = $this->post('/login', $user);
+
+        $response->assertSessionHasErrorsIn('email password');
+
     }
 
 
